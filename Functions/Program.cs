a69d344a5ecf7271fc98cb0d3ba5b16c59c02f51
Kristjan Palm <kpalm@tktk.ee>
2023-03-26 17:36:55 +0300

    add Functions code

---
+++ Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoise = Console.ReadLine();
+
+if(userChoise != "in")
+{
+    PrintHello();
+
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+PrintHello();
+PrintGoodBye();
+
+
+
+
+static void PrintHello() // function / method
+{
+    Console.WriteLine("Hello, world");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
